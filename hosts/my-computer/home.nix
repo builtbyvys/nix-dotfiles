5d8dc0c1ef3de3492d6802ced6f1b152c1611b7c
vys <hey@vyslix.dev>
2024-12-29 17:19:45 -0500

    feat(fish): migrated from bash to fish 🐟

--- hosts/my-computer/home.nix
+++ hosts/my-computer/home.nix
@@ -166,33 +166,28 @@ in
             enable = true;
             package = pkgs.starship;
      };
-    bash = {
-      enable = true;
-      enableCompletion = true;
-      profileExtra = ''
-        #if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
-        #  exec Hyprland
-        #fi
-      '';
-      initExtra = ''
-        fastfetch
-        if [ -f $HOME/.bashrc-personal ]; then
-          source $HOME/.bashrc-personal
-        fi
-      '';
-      shellAliases = {
-        sv = "sudo nvim";
-        fr = "nh os switch --hostname ${host} /home/${username}/nix";
-        fu = "nh os switch --hostname ${host} --update /home/${username}/nix";
-        ncg = "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
-        v = "nvim";
-        cat = "bat";
-        ls = "eza --icons";
-        ll = "eza -lh --icons --grid --group-directories-first";
-        la = "eza -lah --icons --grid --group-directories-first";
-        ".." = "cd ..";
-      };
-    };
+     fish = {
+        enable = true;
+        shellInit = ''
+          fastfetch
+        '';
+        interactiveShellInit = ''
+        if test -f ~/.config/fish/config.fish-personal
+          source ~/.config/fish/config.fish-personal
+        end
+        '';
+        shellAliases = {
+          sv = "sudo nvim";
+          fr = "nh os switch --hostname ${host} /home/${username}/nix";
+          fu = "nh os switch --hostname ${host} --update /home/${username}/nix";
+          ncg = "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
+          v = "nvim";
+          cat = "bat";
+          ll = "eza -lh --icons --grid --group-directories-first";
+          la = "eza -lah --icons --grid --group-directories-first";
+           ".." = "cd ..";
+         };
+     };
     home-manager.enable = true;
     hyprlock = {
       enable = false;
