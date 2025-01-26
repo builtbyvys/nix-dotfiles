eb4805fee448dcce2f19a87281ba728e2ab49229
vys <hey@vyslix.dev>
2025-01-25 19:31:39 -0500

    fix: remove pinned tarball, we made it to nixos-unstable

--- modules/sunshine.nix
+++ modules/sunshine.nix
@@ -1,16 +1,8 @@
 { config, lib, pkgs, ... }:
 
 let
-  nixpkgsTarball = builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/2364607ec91c0aa8f5028aead070ead6da27007b.tar.gz";
-    sha256 = "04fsylz7f8770cykk0wvdkp3ml5pqha3l3xlzpqb26w3zxcmkyl5";
-  };
-  pkgs-master = import nixpkgsTarball {
-    system = builtins.currentSystem or "x86_64-linux";
-    config = { allowUnfree = true; };
-  };
   cfg = config.programs.sunshine;
-  sunshineWithCuda = pkgs-master.sunshine.override {
+  sunshineWithCuda = pkgs.sunshine.override {
     cudaSupport = true;
     stdenv = pkgs.cudaPackages.backendStdenv;
   };
