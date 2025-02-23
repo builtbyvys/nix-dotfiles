3d7c0e169a515a44bf6cf1486e0b9bb627be4b90
vys <hey@vyslix.dev>
2025-02-23 17:45:54 -0500

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -35,72 +35,69 @@
     catppuccin.url = "github:catppuccin/nix";
   };
 
-  outputs =
-    {
-      nixpkgs,
-      self,
-      catppuccin,
-      home-manager,
-      ...
-    }@inputs:
-    let
-      username = "yamil";
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
+  outputs = {
+    nixpkgs,
+    self,
+    catppuccin,
+    home-manager,
+    ...
+  } @ inputs: let
+    username = "yamil";
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+      overlays = [
+        (self: super: {
+          # override cxxopts to use unmerged PR
+          cxxopts = super.cxxopts.overrideAttrs (oldAttrs: {
+            src = builtins.fetchGit {
+              url = "https://github.com/builtbyvys/nixpkgs";
+              ref = "fix-cxxopts-build";
+            };
+          });
+        })
+      ];
+    };
+    lib = nixpkgs.lib;
+  in {
+    # NixOS configurations for different machines
+    nixosConfigurations = {
+      desktop = nixpkgs.lib.nixosSystem {
         inherit system;
-        config.allowUnfree = true;
-        overlays = [
-          (self: super: {
-            # override cxxopts to use unmerged PR
-            cxxopts = super.cxxopts.overrideAttrs (oldAttrs: {
-              src = builtins.fetchGit {
-                url = "https://github.com/builtbyvys/nixpkgs";
-                ref = "fix-cxxopts-build";
-              };
-            });
-          })
+        modules = [
+          ./hosts/desktop
+          catppuccin.nixosModules.catppuccin
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.users.yamil = {
+              imports = [
+                catppuccin.homeManagerModules.catppuccin
+              ];
+            };
+          }
         ];
+        specialArgs = {
+          host = "desktop";
+          inherit self inputs username;
+        };
       };
-      lib = nixpkgs.lib;
-    in
-    {
-      # NixOS configurations for different machines
-      nixosConfigurations = {
-        desktop = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [
-            ./hosts/desktop
-            catppuccin.nixosModules.catppuccin
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.users.yamil = {
-                imports = [
-                  catppuccin.homeManagerModules.catppuccin
-                ];
-              };
-            }
-          ];
-          specialArgs = {
-            host = "desktop";
-            inherit self inputs username;
-          };
+      laptop = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [./hosts/laptop];
+        specialArgs = {
+          host = "laptop";
+          inherit self inputs username;
         };
-        laptop = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [ ./hosts/laptop ];
-          specialArgs = {
-            host = "laptop";
-            inherit self inputs username;
-          };
-        };
-        vm = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [ ./hosts/vm ];
-          specialArgs = {
-            host = "vm";
-            inherit self inputs username;
-          };
+      };
+      vm = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [./hosts/vm];
+        specialArgs = {
+          host = "vm";
+          inherit self inputs username;
         };
       };
     };
+  };
 }
