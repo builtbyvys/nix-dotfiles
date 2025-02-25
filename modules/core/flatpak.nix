afccbf1acd35cbf180f5e71cb35eb280eaa42cfd
vys <hey@vyslix.dev>
2025-02-24 19:10:36 -0500

    update flakes & add weekly flatpak updates

--- modules/core/flatpak.nix
+++ modules/core/flatpak.nix
@@ -4,6 +4,10 @@
 
   services.flatpak = {
     enable = true;
+    update.auto = {
+      enable = true;
+      onCalendar = "weekly";
+    };
     packages = [
       # "io.github.zen_browser.zen"
       "com.github.tchx84.Flatseal"
