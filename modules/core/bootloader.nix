d6a8dccb37f30961a81b9d5b0c3f6e6e67fd8f34
vys <hey@vyslix.dev>
2025-03-08 11:56:41 -0500

    add niv and use lanzaboote for secure boot

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -1,13 +1,40 @@
-{ pkgs, ... }:
 {
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.systemd-boot.configurationLimit = 10;
-  boot.kernelPackages = pkgs.linuxPackages_cachyos;
-  services.scx.enable = true;
-  boot.supportedFilesystems = [ "ntfs" ];
-  boot.kernelModules = [
-    "i2c-dev"
-    "i2c-piix4"
+  pkgs,
+  lib,
+  ...
+}: let
+  sources = import ../../nix/sources.nix;
+  lanzaboote = import sources.lanzaboote;
+in {
+  imports = [lanzaboote.nixosModules.lanzaboote];
+
+  environment.systemPackages = [
+    pkgs.sbctl
   ];
+
+  boot = {
+    kernelPackages = pkgs.linuxPackages_cachyos;
+    kernelModules = [
+      "i2c-dev"
+      "i2c-piix4"
+    ];
+    kernel.sysctl = {
+      "vm.max_map_count" = 2147483642;
+    };
+
+    loader = {
+      systemd-boot.enable = lib.mkForce false;
+      efi.canTouchEfiVariables = true;
+    };
+
+    lanzaboote = {
+      enable = true;
+      pkiBundle = "/var/lib/sbctl";
+      configurationLimit = 10;
+    };
+
+    supportedFilesystems = ["ntfs"];
+  };
+
+  services.scx.enable = true;
 }
