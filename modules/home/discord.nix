50e4f56dbded0bde47784d0aa0377da3bcaee4e2
vys <hey@vyslix.dev>
2025-03-15 15:10:13 -0400

    feat: add nixcord to home-manager
    - remove unused plugins
    - formatted discord.nix
    - refactored config
    - switch to vencord unstable

--- modules/home/discord.nix
+++ modules/home/discord.nix
@@ -1,50 +1,32 @@
-{
-  config,
-  inputs,
-  lib,
-  ...
-}:
+{ ... }:
 {
   programs.nixcord = {
     enable = true;
+    discord = {
+      vencord = {
+        enable = true;
+        unstable = true;
+      };
+      openASAR.enable = true;
+    };
 
-    quickCss =
-      let
-        catppuccinTheme = builtins.readFile "${inputs.catppuccin}/catppuccin-mocha.theme.css";
-      in
-      ''
-        ${catppuccinTheme}
-
-        /* additional css snippets go here */
-        .container-1eFtFS {
-          background-color: rgba(0, 0, 0, 0.6);
-        }
-      '';
+    quickCss = ''
+      /* additional css snippets go here */
+      .container-1eFtFS {
+        background-color: rgba(0, 0, 0, 0.6);
+      }
+    '';
 
     config = {
       useQuickCss = true;
-
       frameless = true;
-
+      enableReactDevtools = true;
       plugins = {
         hideAttachments.enable = true;
+        betterRoleContext.enable = true;
         betterRoleDot.enable = true;
         noDevtoolsWarning.enable = true;
-        notifications.enable = true;
-
-        ignoreActivities = {
-          enable = true;
-          ignorePlaying = true;
-          ignoreWatching = true;
-          ignoredActivities = [
-            "Spotify"
-            "Visual Studio Code"
-          ];
-        };
-
-        readAllNotificationsButton.enable = true;
         betterFolders.enable = true;
-        favoriteMessages.enable = true;
         viewRaw.enable = true;
       };
     };
@@ -53,15 +35,9 @@
       notifyAboutUpdates = true;
       autoUpdate = true;
       autoUpdateNotification = true;
-      useQuickCss = true;
       themeLinks = [ ];
       enabledThemes = [ ];
       enableReactDevtools = false;
     };
   };
-
-  # remove Discord from packages if it was there
-  home.packages = lib.mkIf (config.programs.nixcord.enable) (
-    lib.filter (p: p.pname != "discord") config.home.packages
-  );
 }
