d202b7be3ceab3e24523e4d59d696fbc5a6f08e8
vys <hey@vyslix.dev>
2025-03-22 17:40:51 -0400

    bye gtk

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -13,7 +13,6 @@
     ./ghostty.nix # terminal
     ./git.nix # version control
     ./gnome.nix # gnome apps
-    ./gtk.nix # gtk theme
     ./jellyfin.nix
     ./kitty.nix # terminal
     ./lazygit.nix
