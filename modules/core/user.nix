269a7ee3338074a479a9d43d22060900b29d15ef
vys <hey@vyslix.dev>
2025-05-28 20:53:09 -0400

    add docker virtualization

--- modules/core/user.nix
+++ modules/core/user.nix
@@ -24,6 +24,24 @@
     };
   };
 
+  virtualisation.docker = {
+    # Consider disabling the system wide Docker daemon
+    enable = false;
+
+    rootless = {
+      enable = true;
+      setSocketVariable = true;
+      # Optionally customize rootless Docker daemon settings
+      daemon.settings = {
+        dns = [
+          "1.1.1.1"
+          "8.8.8.8"
+        ];
+        registry-mirrors = [ "https://mirror.gcr.io" ];
+      };
+    };
+  };
+
   users.users.${username} = {
     isNormalUser = true;
     description = "${username}";
