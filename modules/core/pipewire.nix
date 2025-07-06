49745c1bdd0298a13b6078aa1092b198c8bfc17b
vys <hey@vyslix.dev>
2025-07-06 15:21:52 -0400

    feat(audio): enhance performance with low-latency
    - configure pipewire core with 32/48000 quantum for 0.667 latency
    - add PulseAudio backend low-latency settings for compatibility

--- modules/core/pipewire.nix
+++ modules/core/pipewire.nix
@@ -6,7 +6,37 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # lowLatency.enable = true;
+    lowLatency.enable = true;
+
+    # https://nixos.wiki/wiki/PipeWire#Low-latency_setup
+    extraConfig.pipewire."92-low-latency" = {
+      context.properties = {
+        default.clock.rate = 48000;
+        default.clock.quantum = 32;
+        default.clock.min-quantum = 32;
+        default.clock.max-quantum = 32;
+      };
+    };
+
+    extraConfig.pipewire-pulse."92-low-latency" = {
+      context.modules = [
+        {
+          name = "libpipewire-module-protocol-pulse";
+          args = {
+            pulse.min.req = "32/48000";
+            pulse.default.req = "32/48000";
+            pulse.max.req = "32/48000";
+            pulse.min.quantum = "32/48000";
+            pulse.max.quantum = "32/48000";
+          };
+        }
+      ];
+      stream.properties = {
+        node.latency = "32/48000";
+        resample.quality = 1;
+      };
+    };
   };
+
   environment.systemPackages = with pkgs; [ pulseaudioFull ];
 }
