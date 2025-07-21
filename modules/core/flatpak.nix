d2f9dbc5be3e0982bfcf83f19c09d11b8e57ae7f
vys <hey@vyslix.dev>
2025-07-21 14:14:11 -0400

    obtain sober flatpak from flathub

--- modules/core/flatpak.nix
+++ modules/core/flatpak.nix
@@ -12,10 +12,7 @@
       # "io.github.zen_browser.zen"
       "com.github.tchx84.Flatseal"
       "io.github.everestapi.Olympus"
-      {
-        flatpakref = "https://sober.vinegarhq.org/sober.flatpakref";
-        sha256 = "54546f6e843b219c180d0bc47168a63ae9e8eef223fb9133b4ebf1087bf048de";
-      }
+      "org.vinegarhq.Sober"
     ];
     overrides = {
       global = {
